6f115fa7cf43f419a80992f0971485d5b9ec9e50
François de Metz <francois@2metz.fr>
2022-01-24 07:15:36 +0100

    Add gandi_forward table.

---
+++ docs/tables/gandi_forward.md
@@ -0,0 +1,16 @@
+# Table: gandi_forward
+
+The `gandi_foward` table can be used to query information about your email forwards and you must specify which domain in the where or join clause using the `domain` column.
+
+## Examples
+
+### List formards of a domain
+
+```sql
+select
+  source, destinations
+from
+  gandi_forward
+where
+  domain='example.net';
+```
---
+++ gandi/table_gandi_forward.go
@@ -0,0 +1,47 @@
+package gandi
+
+import (
+	"context"
+
+	"github.com/go-gandi/go-gandi"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+func tableGandiForward() *plugin.Table {
+	return &plugin.Table{
+		Name:        "gandi_forward",
+		Description: "List gandi email forwards.",
+		List: &plugin.ListConfig{
+			KeyColumns: plugin.SingleColumn("domain"),
+			Hydrate:    listForward,
+		},
+		Columns: []*plugin.Column{
+			{Name: "domain", Type: proto.ColumnType_STRING, Transform: transform.FromQual("domain"), Description: "Domain name."},
+
+			{Name: "source", Type: proto.ColumnType_STRING, Description: "The source email address."},
+			{Name: "destinations", Type: proto.ColumnType_JSON, Description: "A list of email addresses."},
+
+			{Name: "href", Type: proto.ColumnType_STRING, Description: "URL to forwarding address."},
+		},
+	}
+}
+
+func listForward(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	config, err := connect(ctx, d)
+	client := gandi.NewEmailClient(*config)
+
+	if err != nil {
+		return nil, err
+	}
+	domain := d.KeyColumnQuals["domain"].GetStringValue()
+	mailboxes, err := client.GetForwards(domain)
+	if err != nil {
+		return nil, err
+	}
+	for _, mailbox := range mailboxes {
+		d.StreamListItem(ctx, mailbox)
+	}
+	return nil, nil
+}
